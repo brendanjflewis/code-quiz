3d9c1817f74bd7bdb01d2542ec803aba4988ecbb
brendanjlewis <brendan.j.f.lewis@gmail.com>
2022-02-08 23:47:27 -0500

    attempt #2 to actual code plus basic centering for css

---
+++ script.js
@@ -0,0 +1,49 @@
+const questions = [
+    {
+        question: "Commonly used data types do NOT include:",
+        options: [
+            "strings", 
+            "booleans", 
+            "alerts", 
+            "numbers"
+        ],
+        correct: "strings"
+    }, 
+    {
+        question: "The condition with an if / else statement is enclosed with ___",
+        options: [
+            'quotes', 
+            'curly brackets', 
+            'perenthesis', 
+            'square brackets'
+        ],
+        correct: "square brackets",
+    }, 
+    {
+        question: "Arrays in Javascript can be used to store ___",
+        options: [
+            'numbers and strings', 
+            'other arrays', 
+            'booleans', 
+            'all of the above'
+        ],
+        correct: "other arrays"
+    }, 
+    {
+        question: "String values most be enclosed within ___ when being associated to variables",
+        options: [
+            'commas',
+            'curly brackets',
+            'quotes',
+            'perenthesis'],
+        correct: "curly brackets"
+    }, 
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: ['JavaScript',
+        'terminal/bash',
+        'for loops',
+        'console.log'],
+        correct: "for loops"
+    }
+]
